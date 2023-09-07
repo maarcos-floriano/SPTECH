8f56128640d3d651eb5829b0b85d6966e95875cc
maarcos01 <toretomarcos50@gmail.com>
2023-09-07 12:50:10 -0300

    nao sei

---
+++ Banco de Dados/script-continuada.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE continuada1;
+
+USE continuada1;
+
+CREATE TABLE historico (
+id INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(200) NOT NULL,
+dataHora DATETIME DEFAULT current_timestamp,
+url VARCHAR(250) NOT NULL,
+navegador VARCHAR(100)
+);
+
+INSERT INTO historico (nome, url, navegador) VALUES
+	('São Paulo Tech School', 'https://moodle.sptech.school/', 'Microsoft Edge'),
+	('Página Inicial - Microsoft Azure', 'https://portal.azure.com/#home', 'Microsoft Edge'),
+	('Ofertas e Promoções | Amazon.com.br', 'https://www.amazon.com.br/deals?ref_=nav_cs_gb', 'Firefox Developer Edition'),
+	('404 Not Found', 'http://moodletech.azurewebsites.net/', 'Firefox Developer Edition'),
+	('Working...', 'https://login.microsoftonline.com/kmsi', 'Firefox Developer Edition'),
+	('Email - Usuario Aluno - Outlook', 'https://outlook.office.com/mail/', 'Firefox Developer Edition'),
+	('Solicitações - Suporte - Digital Building', 'https://bandtec.sdpondemand.manageengine.com/app/itdesk/ui/requests', 'Firefox Developer Edition'),
+	('Carrinho de compras da Amazon.com', 'https://www.amazon.com.br/gp/cart/view.html?ref_=nav_cart', 'Firefox Developer Edition'),
+	('MySQL | Begin Your Download', 'https://dev.mysql.com/downloads/file/?id=516926', 'Firefox Developer Edition'),
+	('MySQL | Download MySQL Installer', 'https://dev.mysql.com/downloads/installer/', 'Firefox Developer Edition');
